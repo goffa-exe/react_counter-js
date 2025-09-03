6ba10f03707955a4a80b581e3b21eff319896536
gOFFa <n4v4rm0re666@gmail.com>
2025-09-03 14:40:02 +0300

    fix increase logic

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,7 @@ export const App = () => {
     setCount(prev => {
       const next = prev + 1;
 
-      if (prev % 5 === 0) {
-        return next + 100;
-      }
-
-      return next;
+      return next % 5 === 0 ? next + 100 : next;
     });
   };
 
